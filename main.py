e1bdb4e8442189323cb843abd0596f08b64fb727
NourA <nr858090@dal.ca>
2023-06-28 09:42:17 -0300

    Removed redundant files, trying a different approach

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from website import create_app
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
+@app.route('/', methods=['GET', 'POST'])
+def home(var):
+    # data = request.form
+    # data = str(data.get(key='url'))
+    # if type(data) == str and len(data) > 1:
+    #     driver.get(data)
+    return f"Hello {var}!"
+
+if __name__ == "__main__":
     app.run(debug=True)
--- website/auth.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-
-@auth.route('/')
-def home():
-    return "<h1>Hello</h1>"
