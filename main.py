24dbdfba689750abf2a7b18adcce590eb3f7d27b
nallam <nallam@dal.ca>
2023-06-30 11:40:52 -0300

    URL-redirect form operational, added css

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, render_template, redirect
+from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 
 
@@ -6,8 +6,18 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         url = request.form["nm"]
-        return redirect(url)
+        if url[0:3] == "www":
+            return redirect("https://" + url)
+        elif url[0:8] == "https://":
+            return redirect(url)
+        else:
+            return redirect("https://www." + url)
     return render_template("index.html")
+
+
+@app.errorhandler(401)
+def page_not_found(error):
+    return render_template("error.html")
 
 
 if __name__ == "__main__":
